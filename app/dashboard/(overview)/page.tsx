d16d65aa813bfe1114cf911b81ad4a4d8b485dab
MohitKate <mohitkate1611@gmail.com>
2025-03-24 16:42:37 +0530

    streaming the page and component using suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -27,7 +27,6 @@ export default async function Dashboard() {
         <Suspense fallback={<RevenueChartSkeleton />}>
         <LatestInvoices />
          </Suspense>
-
       </div>
     </main>
     
